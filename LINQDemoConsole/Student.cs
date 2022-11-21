5b5d9f6dd3688d4248599d095b05b91febd5da84
Aayushi Shukla <AayushiShukla27@gmail.com>
2022-11-21 14:41:05 -0500

    Added missing files

---
+++ LINQDemoConsole/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemoConsole
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentFullName { get; set; }
+        public string StudentEmail { get; set; }
+    }
+}
